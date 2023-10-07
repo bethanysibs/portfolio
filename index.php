d03b1d8f32a3da5e7bd25e302e9d77a00003e981
Bethany Joyce Sibala <bethanyjoycesibala@gmail.com>
2023-10-07 15:50:15 +0800

    asdfadfs

--- index.php
+++ index.php
@@ -41,7 +41,7 @@
         </div>
     </div>
     <!---->
-    <div class="h-20 bg-black flex p-4 justify-between">
+    <div class="h-20 bg-black flex justify-between items-center">
          <div class="github">
             <img src="assets/img/logo-light.png" width="150px">
         </div>
@@ -64,18 +64,46 @@
 
 <!--end of section 1-->
 <!--section 2-->
-<div class="h-screen homeBg flex flex-col items-center space-y-16">
+<div class="h-full homeBg flex flex-col items-center space-y-8">
     <div class="flex mt-12 justify-center w-full space-x-20">
         <div>
             <img class="img2" src="assets/img/beth.png">
         </div>            
         <div class="w-1/2 space-y-4 mt-10">
-            <h2 class="section2 uppercase">Frontend Developer currently based in the Philippines</h2>
-            <h4 class="text w-2/3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.</h4>
+            <h2 class="content uppercase">Frontend Developer currently based in the Philippines</h2>
+            <h4 class="content w-2/3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.</h4>
         </div>
     </div>
+
     <hr>
+
+    <div class="flex justify-center w-full px-16 space-x-8 items-center pt-8">
+        <div class="w-1/2 px-8">
+            <h2 class="content uppercase">WHAT DO I HELP</h2>
+            <h4 class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus..</h4>
+            
+        </div>
+        <div  class="w-1/2 px-8">
+           <div class="flex flex-col space-y-10">
+                <div class="box bg-gray-700 p-4 flex items-center space-x-4">
+                    <img class="icons" src="assets/img/uiDesign.png">
+                    <h5 class="content">User Interface Design</h5>
+                </div>
+                <div class="box bg-gray-700 p-4 flex items-center space-x-4">
+                    <img class="icons" src="assets/img/uiDesign.png">
+                    <h5 class="content">User Interface Design</h5>
+                </div>
+                <div class="box bg-gray-700 p-4 flex items-center space-x-4">
+                    <img class="icons" src="assets/img/uiDesign.png">
+                    <h5 class="content">User Interface Design</h5>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </div>
+<!--end section 2-->
+
 
 
     
