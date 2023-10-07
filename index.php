c5f48031a29f87f574acd94444f00079b84ae81f
Bethany Joyce Sibala <bethanyjoycesibala@gmail.com>
2023-10-07 14:24:25 +0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bethany Sibala - Frontend Developer</title>
+    <link rel="stylesheet" href="style/style.css">
+    <link rel="stylesheet" href="style/output.css">
+</head>
+<body>
+
+<!--section 1-->
+<div class="h-screen homeBg flex flex-col">
+    <nav class="flex p-4">
+        <div class="logo">
+            <img src="assets/img/logo-light.png" width="150px">
+        </div>
+        <div class="flex justify-end w-full">
+            <ul class="navItems flex items-center space-x-6 text-md">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Services</a></li>
+                <li class="flex">
+                    <button class="border border-sky-500 px-2 py-1 font-semibold">Hire Me</button>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <!---->
+    <div class="h-screen flex items-end justify-end">
+        <div class="flex-col mb-8">            
+            <div>
+                <h1 class="name">I'm Bethany Sibala.</h1>
+            </div>
+            <div>
+                <span>Frontend Web Developer</span>
+            </div>
+        </div>
+        <div>
+            <img class=".beth" src="assets/img/beth.png">
+        </div>
+    </div>
+</div>
+
+
+<!--end of section 1-->
+    
+</body>
+</html>
